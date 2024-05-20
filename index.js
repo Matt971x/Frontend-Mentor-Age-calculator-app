8097d5dd34bbdec16d73629fc9b95583e5ef2a12
“Matt971x“ <matthieu.rosan@gmail.com>
2024-05-20 17:33:07 +0200

    dekstop and mobile design done js modified

--- index.js
+++ index.js
@@ -1,60 +1,92 @@
-const inputDay = document.getElementById("day");
-const inputMonth = document.getElementById("month");
-const inputYear = document.getElementById("year");
-const inputResult = document.querySelector(".result");
+let years;
+let months;
+let days;
 
-let currentYear = new Date().getFullYear();
-let currentMonth = new Date().getMonth();
-let current_date = new Date();
+const ageCalculator = () => {
+  user_date = new Date(years, months - 1, days);
+  let current_date = new Date();
+  diff = current_date - user_date;
+  let age = new Date(diff);
+  let year = Math.abs(age.getUTCFullYear() - 1970);
+  let month = age.getUTCMonth();
+  let day = age.getUTCDate() - 1;
 
-const daysInMonth = (year, month) => new Date(year, month, 0).getDate();
-let day;
-let month;
-let year;
+  document.querySelector(".years").textContent = year;
+  document.querySelector(".months").textContent = month;
+  document.querySelector(".days").textContent = day;
+};
 
-inputDay.addEventListener("input", (e) => {
-  if (isNaN(e.target.value)) {
-    console.log("erreur");
+const inputYear = document.querySelector("#year");
+const inputMonth = document.querySelector("#month");
+const inputDay = document.querySelector("#day");
+const submitBtn = document.querySelector(".submitBtn");
+
+inputYear.addEventListener("input", (e) => {
+  if (
+    isNaN(e.target.value) ||
+    e.target.value > new Date().getFullYear() ||
+    e.target.value === ""
+  ) {
+    e.target.value > new Date().getFullYear()
+      ? (document.querySelector(".error_year").textContent =
+          "must be a valid paste")
+      : (document.querySelector(".error_year").textContent =
+          "must be a valid date");
+    console.log("error");
+    years = null;
   } else {
-    day = e.target.value;
-    console.log(e.target.value);
+    document.querySelector(".error_year").textContent = "";
+    years = parseInt(e.target.value);
   }
 });
 
 inputMonth.addEventListener("input", (e) => {
-  if (isNaN(e.target.value)) {
-    console.log("erreur");
+  if (
+    isNaN(e.target.value) ||
+    e.target.value > 12 ||
+    e.target.value < 1 ||
+    e.target.value === ""
+  ) {
+    e.target.value > 12
+      ? (document.querySelector(".error_month").textContent =
+          "Must be a valid month")
+      : (document.querySelector(".error_month").textContent =
+          " must me a valid date");
+    months = null;
   } else {
-    month = e.target.value;
-    console.log(e.target.value);
+    months = parseInt(e.target.value);
+    document.querySelector(".error_month").textContent = "";
   }
 });
 
-inputYear.addEventListener("input", (e) => {
-  if (isNaN(e.target.value)) {
-    document.querySelector(".error").textContent = "erreur";
+inputDay.addEventListener("input", (e) => {
+  if (
+    isNaN(e.target.value) ||
+    e.target.value > 31 ||
+    e.target.value < 1 ||
+    e.target.value === ""
+  ) {
+    document.querySelector(".error_day").textContent = "must be a valid date";
+    days = null;
   } else {
-    year = e.target.value;
-    console.log(e.target.value);
+    days = parseInt(e.target.value);
+    document.querySelector(".error_day").textContent = "";
   }
 });
 
-const ageCalculator = () => {
-  let user_date = new Date(year, month, day);
-  let totalOfDayLeft = (current_date - user_date) / 86400000;
-  let totalMonthLeft = 12 - (user_date.getMonth() - current_date.getMonth());
-  let numberOfYear = Math.floor(totalOfDayLeft / 365);
-  let numberOfDay = current_date.getUTCDate() - user_date.getUTCDate();
-  if (numberOfDay <= 0) {
-    document.querySelector(".days").textContent =
-      daysInMonth(currentYear, currentMonth) + numberOfDay;
-    numberOfYear--;
+submitBtn.addEventListener("click", (e) => {
+  if (years === undefined || months === undefined || days === undefined) {
+    document.querySelector(".error_day").textContent =
+      "This filed is requiered";
+    document.querySelector(".error_month").textContent =
+      "This filed is requiered";
+    document.querySelector(".error_year").textContent =
+      "This filed is requiered";
   } else {
-    document.querySelector(".days").textContent = numberOfDay;
+    if (years === null || months === null || days === null) {
+      alert("Please enter a valid date.");
+    } else {
+      ageCalculator();
+    }
   }
-  document.querySelector(".years").innerHTML = numberOfYear;
-  document.querySelector(".months").textContent =
-    totalMonthLeft >= 12 ? totalMonthLeft - 12 : totalMonthLeft;
-};
-
-inputResult.addEventListener("click", ageCalculator);
+});
